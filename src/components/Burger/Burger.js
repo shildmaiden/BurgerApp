4805537ef1751bcb80dd08c05d9af7ada516fdd8
Vojislav Stanisic <stanisicvojislav@gmail.com>
2019-04-03 12:07:57 +0200

    adding state into BurgerBuilder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+    const transformedIngredient = Object.keys(props.ingredients)
+        .map(item => {
+            return [...Array(props.ingredients[item])].map((_, i) => {
+                return <BurgerIngredient key={item + i} type={item} />
+            });
+        })
 
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredient}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
